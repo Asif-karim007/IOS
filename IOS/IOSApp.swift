0de8ee734304840123662cd67d4f951fbaf74c2e
Asif Karim <asif0171797@gmail.com>
2022-02-01 16:23:20 +0600

    first commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Asif Karim on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
