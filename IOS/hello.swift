a682fabf4ee83c79acf78ae76b471a6e8ee8145f
Asif Karim <asif0171797@gmail.com>
2022-03-16 18:09:10 +0600

    hello

---
+++ IOS/hello.swift
@@ -0,0 +1,20 @@
+//
+//  hello.swift
+//  IOS
+//
+//  Created by Asif Karim on 3/16/22.
+//
+
+import SwiftUI
+
+struct hello: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct hello_Previews: PreviewProvider {
+    static var previews: some View {
+        hello()
+    }
+}
