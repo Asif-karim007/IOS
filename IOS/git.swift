181b69281d41b3d4fb21707b07b45eca723238cd
Asif Karim <asif0171797@gmail.com>
2022-03-16 19:17:50 +0600

    Created git

---
+++ IOS/git.swift
@@ -0,0 +1,20 @@
+//
+//  git.swift
+//  IOS
+//
+//  Created by Asif Karim on 3/16/22.
+//
+
+import SwiftUI
+
+struct git: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct git_Previews: PreviewProvider {
+    static var previews: some View {
+        git()
+    }
+}
